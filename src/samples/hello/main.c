4a8064538e3da025359f3ef65f6820dc3dfa4159
root <root@localhost.localdomain>
2019-04-09 13:43:30 +0800

    test with redis

--- src/samples/hello/main.c
+++ src/samples/hello/main.c
@@ -18,14 +18,70 @@
  */
 
 #include "lwan.h"
+#include "hiredis.h"
+#include <pthread.h>
+#include <adapters/libuv.h>
+
+pthread_mutex_t *mutex = NULL;
 
 LWAN_HANDLER(hello_world)
 {
-    static const char message[] = "Hello, World!";
+    int result = 0;
 
-    response->mime_type = "text/plain";
-    lwan_strbuf_set_static(response->buffer, message, sizeof(message) - 1);
+    redisContext *c = redisConnect("127.0.0.1", 6379);
+    // redisContext *c = redisConnect("192.168.1.204", 6379);
+    redisReply *reply = NULL;
+    
+    // pthread_mutex_lock(mutex);
 
+    if (c == NULL || c->err) {
+        result = 1;    
+    } else {
+        reply = redisCommand(c,"lpop phone");
+        if (!reply) {
+            result = 1;
+        } else {
+            redisReply *replypush = NULL;
+            printf("reply->str:%s\n", reply->str);
+            replypush = redisCommand(c,"rpush phone %s",reply->str);
+            if (!replypush) {
+                result = 1;
+            } else {
+                result = 0;
+                freeReplyObject(replypush);
+            }
+
+            freeReplyObject(reply);
+        }
+    }
+
+    // unsigned int a = 0;
+    // for (int k = 0; k < 100000; ++k) {
+    //     for (int i = 0; i < 100000; ++i) {
+    //         for (int j = 0; j < 100000; ++j) {
+    //             a += 1;
+    //         }
+    //     }
+    // }
+    // printf("threadid:%ld,%ld\n", gettid(),a);
+    
+    if (result == 0) {
+        static const char message[] = "SUCCESS!";
+        response->mime_type = "text/plain";
+        lwan_strbuf_set_static(response->buffer, message, sizeof(message) - 1);        
+    
+    } else {
+        static const char message[] = "ERROR!";
+        response->mime_type = "text/plain";
+        lwan_strbuf_set_static(response->buffer, message, sizeof(message) - 1);                
+        printf("err!!\n");
+    }
+
+    if (c) {
+        redisFree(c);
+    }
+
+    // pthread_mutex_unlock(mutex);
     return HTTP_OK;
 }
 
@@ -37,6 +93,12 @@ main(void)
         { .prefix = NULL }
     };
     struct lwan l;
+    mutex = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(mutex,NULL);
+    if (!mutex) {
+        printf("err init mutex\n");
+    }
+    
 
     lwan_init(&l);
 
